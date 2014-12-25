0879fe1394ac0629b078838875fffbf690bccf4e
Zakaria Soliman <zakaria.soliman1@gmail.com>
2014-12-24 21:08:11 -0500

    modified makessymlinks

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -24,8 +24,10 @@ echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    echo "Copying .$file from ~ to $olddir for backup"
+    cp  ~/.$file ~/dotfiles_old/
+    echo "Moving dotfiles from ~ to $dir"
+    mv ~/.$file $dir/$file
     echo "Creating symlink to $file in home directory."
     if [ $file == "vimrc" ]
         then
---
+++ vimrc
@@ -0,0 +1,1 @@
+/home/danglewaggle/dotfiles/vim/vimrc
