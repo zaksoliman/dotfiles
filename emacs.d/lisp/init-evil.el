9cfe4cda49917fdd2913c44477048d71d77ba411
Zak <zakarias@paradox.com>
2020-12-18 10:57:53 -0500

    sync

--- emacs.d/lisp/init-evil.el
+++ emacs.d/lisp/init-evil.el
@@ -10,11 +10,11 @@
 
 (evil-mode 1)
 
- ;(require-package 'evil-org)
- ;(require 'evil-org)
- ;(add-hook 'org-mode-hook 'evil-org-mode)
- ;(evil-org-set-key-theme '(navigation insert textobjects additional calendar))
- ;(require 'evil-org-agenda)
- ;(evil-org-agenda-set-keys)
+(require-package 'evil-org)
+(require 'evil-org)
+(add-hook 'org-mode-hook 'evil-org-mode)
+(evil-org-set-key-theme '(navigation insert textobjects additional calendar))
+(require 'evil-org-agenda)
+(evil-org-agenda-set-keys)
 
 (provide 'init-evil)
--- emacs.d/lisp/init-themes.el
+++ emacs.d/lisp/init-themes.el
@@ -10,7 +10,7 @@
 (setq custom-safe-themes t)
 
 ;; If you don't customize it, this is the theme you get.
-(setq-default custom-enabled-themes '(sanityinc-tomorrow-bright))
+(setq-default custom-enabled-themes '(sanityinc-solarized-dark))
 
 ;; Ensure that themes will be applied even if they have not been customized
 (defun reapply-themes ()
